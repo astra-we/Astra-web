56960b66ace73c89e66b7833cd9f4594fcf5b22e
astra-we <emiv6442@gmail.com>
2026-06-02 06:33:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 const cards = document.querySelectorAll(".card");
 
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
+const observer = new IntersectionObserver((entries) => {
+
+  entries.forEach((entry) => {
+
     if(entry.isIntersecting){
+
       entry.target.style.opacity = "1";
       entry.target.style.transform = "translateY(0)";
+
+      observer.unobserve(entry.target);
+
     }
+
   });
+
+}, {
+  threshold: 0.15
 });
 
-cards.forEach(card => {
+cards.forEach((card) => {
+
   card.style.opacity = "0";
   card.style.transform = "translateY(40px)";
-  card.style.transition = "0.6s";
+  card.style.transition = "all 0.6s ease";
+
   observer.observe(card);
+
 });
